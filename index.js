4dd2391f3407879b6a76edae3d091d4fb8cd4fb8
jana sanjakdar <jana.sanjakdar@hotmail.com>
2021-04-28 21:51:14 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,21 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"},
+  {year: "2013", result: "L"},
+  {year: "2012", result: "W"},
+  {year: "2015", result: "N/A"} ]
+
+function superbowlWin (record){
+  let result = record.find((rec)=> rec.result === 'W');
+  if (result){
+    return result.year;
+  }else {
+    return undefined;
+  }
+}

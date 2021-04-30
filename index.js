cad212e05232873b3b9b3c47309af297c8af8fd6
Salim Al Mersally <salim.almersally@lau.edu>
2021-04-30 20:30:12 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array){
+  let winObj = array.find(function(obj){
+    return obj.result === "W";
+  })
+  if(winObj){
+    return winObj.year
+  } else{
+    return undefined;
+  }
+}

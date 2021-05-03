c3070b89b97c98fbe78414d445b2745060c2bc9a
Nour El Houda Sarmout <nourelhoudasarmout@gmail.com>
2021-05-03 10:54:00 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+const superbowlWin = (arr) => {
+  const winGame = arr.find(obj => obj.result === "W")
+  if(!!winGame) {
+    return winGame.year
+  } else {
+    return undefined
+  }
+}  

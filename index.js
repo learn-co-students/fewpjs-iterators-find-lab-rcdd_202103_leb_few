9c471c08b49fe5cfc96fcb682c51a98eca38bdc5
Mohamad  Al Sabaa <mohamadsabaa3@gmail.com>
2021-05-07 14:30:51 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+function superbowlWin(record) {
+    let winner = record.find(w => w.result === "W");
+     return winner ? winner.year : undefined
+}

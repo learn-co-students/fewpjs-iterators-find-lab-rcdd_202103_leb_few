eb9563dfe95e71ec1799d0d41926c51ae5391a9d
Mahmoud Ajam <mahmoud.aajam@gmail.com>
2021-05-10 18:08:16 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+function superbowlWin(record) {
+    let winner = record.find(winner => winner.result === "W");
+     return winner ? winner.year : undefined
+}
